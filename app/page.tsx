efe7844ec11b2b0098b7e4eb0a53244c7d85408c
sakiadachi <sakiadachi.88@gmail.com>
2023-11-05 22:25:32 +0900

    Add redirect in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+"use client";
 import EmojiList from "./components/EmojiList";
 import AddEmojiForm from "./components/AddEmojiForm";
 import Header from "./components/Header";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    async function authUser() {
+      const response = await fetch("http://localhost:8000/auth/user/", {
+        method: "GET",
+        credentials: "include",
+      });
+      if (response.status === 403) {
+        // Authentication Failed
+        router.replace("/login");
+      }
+    }
+    authUser();
+  }, [router]);
+
   return (
     <main className="min-h-screen max-w-screen-md mx-auto p-10">
       <Header />
